d2729ee2b768d61aecc21119db2f9e3405f31022
Anand <venkataramarajupr@gmail.com>
2025-01-07 22:01:54 +0530

    new changes added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -32,7 +32,7 @@ export class HeaderComponent implements OnInit {
   }
 
   redirectToHomePage() {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     this.router.navigate(['/homefood']);
   }
 
